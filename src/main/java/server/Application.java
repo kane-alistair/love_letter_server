4579444e2a32b0db0af9237e113c9c07d2e253ba
kane-alistair <alistair.kane@and.digital>
2023-11-08 19:44:33 +0000

    test

--- src/main/java/server/Application.java
+++ src/main/java/server/Application.java
@@ -3,6 +3,7 @@ package server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
